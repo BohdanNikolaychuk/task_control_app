d05818b9dc794f8f58a583f83401bf080dad6255
Bohdan <nikolaychukbm@gmail.com>
2022-09-24 14:12:03 +0300

    update styles/add services folder

---
+++ frontend/src/app/core/services/web-request.service/web-request.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebRequestService {
+  public MAIN_URL!: string;
+
+  constructor(private http: HttpClient) {
+    this.MAIN_URL = environment.MIAN_URL;
+  }
+
+  get(url: string) {
+    return this.http.get(`${this.MAIN_URL}/${url}`);
+  }
+  post(url: string, payload: Object) {
+    return this.http.post(`${this.MAIN_URL}/${url}`, payload);
+  }
+  patch(url: string, payload: Object) {
+    return this.http.patch(`${this.MAIN_URL}/${url}`, payload);
+  }
+  delete(url: string) {
+    return this.http.delete(`${this.MAIN_URL}/${url}`);
+  }
+}
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MIAN_URL: 'http://localhost:8080/',
 };
 
 /*
