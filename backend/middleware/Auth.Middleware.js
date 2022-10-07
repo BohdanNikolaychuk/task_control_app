4cd427184e70fdf76e61c7be230e06d68542de82
Bohdan <nikolaychukbm@gmail.com>
2022-10-07 16:34:38 +0300

    fix bug

--- backend/middleware/Auth.Middleware.js
+++ backend/middleware/Auth.Middleware.js
@@ -3,18 +3,16 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) {
+  if (!req.headers) {
     return res.status(401).json({ message: 'Please, provide authorization header' });
   }
 
-  const [, token] = authorization.split(' ');
-
-  if (!token) {
+  if (!authorization) {
     return res.status(401).json({ message: 'Please, include token to request' });
   }
 
   try {
-    const tokenPayload = jwt.verify(token, 'secret-jwt-key');
+    const tokenPayload = jwt.verify(authorization, 'secret-jwt-key');
     req.user = {
       userId: tokenPayload.userId,
       username: tokenPayload.fullName,
