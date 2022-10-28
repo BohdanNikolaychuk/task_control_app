2487234e4d98c42e94fda29dc9816acfcc1fc32a
Bohdan <nikolaychukbm@gmail.com>
2022-10-28 22:44:41 +0300

    test:board and dashboard services

--- frontend/src/app/core/services/board.service/board.service.spec.ts
+++ frontend/src/app/core/services/board.service/board.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
   let service: BoardService;
 
+  let controller: HttpTestingController;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(BoardService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be returned empty array', () => {
+    const resBoards = service.getBoardsValue();
+    expect(resBoards).toEqual([]);
   });
 });
--- frontend/src/app/core/services/dashboard.service/dashboard.service.spec.ts
+++ frontend/src/app/core/services/dashboard.service/dashboard.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { DashboardService } from './dashboard.service';
 
 describe('DashboardService', () => {
   let service: DashboardService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DashboardService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be returned empty array', () => {
+    const resBoards = service.getDashBoardsValue();
+    expect(resBoards).toEqual([]);
   });
 });
